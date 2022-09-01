765294a8db269cce0ef86522d220fe790a6eb06a
senchi <senchpimy@gmail.com>
2022-08-31 19:00:22 -0500

    Divisiones

--- Divisiones/src/main.rs
+++ Divisiones/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let mut s=String::new();
 
     num1=rng.gen_range(10000..999999);
-    num2=rng.gen_range(11..999);
+    num2=rng.gen_range(11..99);
 
     println!("{}", num1);
     println!("{}", num2);
