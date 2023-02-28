465c880b732b5f66ea5b56dc8d88f5be566d65a0
senchi <senchpimy@gmail.com>
2023-02-28 15:41:41 -0600

    update

--- pilacircular/pilacircular.c
+++ pilacircular/pilacircular.c
@@ -3,7 +3,7 @@
 //int pila[10];
 int pila[10]={9,2,3,4,5,6,0,0,0,0};
 int comienzo=0;
-int final=6;
+int final=5;
 
 void printPila(int val,int acc){
 	if (val==0&&acc==0){
@@ -13,7 +13,7 @@ void printPila(int val,int acc){
 		printf("\n");
 	}else if (acc==1){
 		for (int i=0;i<10;i++){
-			if (i==final-1){
+			if (i==final){
 				printf("\033[92;5m%d:\033[0m",pila[i]);
 			}else{
 				printf("%d:",pila[i]);
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 	printPila(0,0);
 	while (1) {
 		do {
-			printf("Escriba A para añadir o E para eliminar: ");
+			printf("Escriba A para añadir, E para eliminar o e para estad o e para estado: ");
 			c = getchar();
 			try=getchar();
 			if (try !='\n'){
@@ -54,33 +54,38 @@ int main(int argc, char *argv[])
 		if (c=='A'){
 			agregar(rand()%50);
 		}
+		if (c=='e'){
+			printPila(0, 0);
+		}
+
 	}
     return 0;
 }
 
 void agregar(int valor){
-	if (final==comienzo){
-		printf("pila LLena\n");
-		printf("Comienzo : %d Final: %d\n",comienzo,final);
-		return;
-	}
-	if (final == 10 && comienzo == 0){
-		printf("pila LLena\n");
-		return;
-	}
+	int tmp = final;
+	final++;
 	if (final == 10){
 		final=0;
 	}
+	if (final==comienzo){
+		printf("pila LLena\n");
+		final=tmp;
+		return;
+	}
 	pila[final]=valor;
-	final++;
 	printPila(valor,1);
 }
 
 void eliminar(){
 	pila[comienzo]=0;
+	int tmp = comienzo;
 	comienzo++;
+	printPila(0,2);
 	if (comienzo==10){
 		comienzo=0;
 	}
-	printPila(0,2);
+	if (comienzo==final+1){
+		comienzo=tmp;
+	}
 }
