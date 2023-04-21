f8cd0c9845220a7fe1b42a75e59b8aec866a3820
senchi <senchpimy@gmail.com>
2023-04-21 17:38:28 -0600

    eliminar linked

--- linked_list/eliminar.cpp
+++ linked_list/eliminar.cpp
@@ -11,7 +11,10 @@ public:
     Nodo* sig;
 };
 
+Nodo* penultimo;
+Nodo* penultimo_pre;
 Nodo* ultimo;
+Nodo* primero_g;
 Nodo* disponible;
 
 void printList(Nodo* n){
@@ -22,15 +25,23 @@ void printList(Nodo* n){
     cout<<"\n";
 }
 
-void agrVal(){
-	if (disponible->sig != NULL){
+void eliminarVal(){
+	if (penultimo != primero_g){
+		penultimo=primero_g;
+		penultimo_pre=primero_g->sig;
+		while (penultimo_pre!=ultimo){
+			penultimo = penultimo_pre;
+			penultimo_pre=penultimo->sig;
+		}
 		ultimo->sig=disponible;
-		disponible->val=rand()%50;
-		ultimo=disponible;
-		disponible=disponible->sig;
+		disponible=ultimo;
+		disponible->val=0;
+		ultimo=penultimo;
 		ultimo->sig=NULL;
 	}else{
-		cout<<" Lista disponible llena\n";
+		primero_g->sig=disponible;
+		primero_g->val=0;
+		cout<<"Lista principal vacia\n";
 	}
 }
 
@@ -66,6 +77,7 @@ int main (int argc, char *argv[])
 	segundo->sig = tercero;
 	tercero->val = 3;
 	/// ultimo
+	primero_g=primero;
 	ultimo=tercero;
 	disponible=cuarto;
 	/// Disponibles
@@ -82,7 +94,7 @@ int main (int argc, char *argv[])
 		cout<< "Quiere agregar un valor? [y/n]";
 		std::getline(std::cin, line);
 		if (line == "y"){
-			agrVal();
+			eliminarVal();
 		}
 		cout<< "Lista principal:";
 		printList(primero);
